9c3fb86c0016d228d84c32c2b3c6e071806d2719
Anastasiia <starkoans@gmail.com>
2025-01-06 14:43:54 +0500

    feat: done 6 of 21 tasks

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+	let count = 0;
+	let s1_arr = s1.split("");
+	let s2_arr = s2.split("");
+	for (let i = 0; i < s1_arr.length; i += 1) {
+		const find_ind = s2_arr.indexOf(s1_arr[i]);
+		if (find_ind >= 0) {
+			count += 1;
+			s2_arr[find_ind] = "*";
+		}
+	}
+	return count;
 }
 
 module.exports = {
-  getCommonCharacterCount
+	getCommonCharacterCount,
 };
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+	let max = 0;
+	const digits = String(n).split("");
+
+	for (let i = 0; i < digits.length; i++) {
+		let curr = Number(digits.slice(0, i).concat(digits.slice(i + 1)).join(''));
+		if (curr> max) {
+			max = curr;
+		}
+	}
+	return max;
 }
 
 module.exports = {
-  deleteDigit
+	deleteDigit,
 };
